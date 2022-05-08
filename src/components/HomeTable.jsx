6609d5830ebc70907aca9805edf72f03ecd67c21
tanvirewu <tanvirewu32@gmail.com>
2022-05-08 14:39:15 +0600

    adding modal for booking & returning

--- src/components/HomeTable.jsx
+++ src/components/HomeTable.jsx
@@ -1,121 +1,150 @@
 import React, { useState } from "react";
 import data from "../Data.json";
+import Modal from "./Modal";
+import ModalReturn from "./ModalReturn";
 
 const HomeTable = () => {
-  // console.log(data)
-  const [input, setInput] = useState('');
-  const [modifiedData, setmodifiedData] = useState(data)
+  const [input, setInput] = useState("");
+  const [modifiedData, setmodifiedData] = useState(data);
+  const [modal, setModal] = useState(false);
 
   const searchFn = () => {
-    setmodifiedData(data.filter(
-      (d) => d.name.toLowerCase().indexOf(input.toLowerCase()) !== -1
-    ))
+    setmodifiedData(
+      data.filter(
+        (d) => d.name.toLowerCase().indexOf(input.toLowerCase()) !== -1
+      )
+    );
     console.log(modifiedData);
   };
 
-
-
   return (
-    <div className="max-w-6xl">
-      {/* SearchBar */}
-      <div>
-        <div className="flex justify-center">
-          <div className="mb-3 xl:w-96">
-            <div className="input-group relative flex items-stretch w-full mb-4">
-              <input
-                className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-black focus:outline-none"
-                placeholder="Search"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-              />
+    <div className="flex justify-center">
+      <div className="max-w-6xl">
+        {/* SearchBar starts here */}
+        <div>
+          <div className="flex justify-end mt-6 mr-[90px]">
+            <div className="mb-3 xl:w-96">
+              <div className="input-group relative flex items-stretch w-full mb-4">
+                <input
+                  className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-black focus:outline-none"
+                  placeholder="Search"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
 
-              <button
-                className="btn inline-block px-6 py-2 border-2 border-black text-orange-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-                type="button"
-                onClick={() => searchFn()}
-              >
-                Search
-              </button>
+                <button
+                  className="btn inline-block px-6 py-2 border-2 border-black text-orange-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                  type="button"
+                  onClick={() => searchFn()}
+                >
+                  Search
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Table */}
-      <div className="flex flex-col mt-5 mx-20 border-8">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-center">
-                <thead className="border-b">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Availability
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Price
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Durability
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Mileage
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4"
-                    >
-                      Needing Repair
-                    </th>
-                  </tr>
-                </thead>
+        {/* SearchBar ends here */}
 
-                {modifiedData.map((d) => (
-                  <tbody>
-                    <tr className="border-b">
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.name}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.availability ? "True" : "False"}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.price}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.durability}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.mileage}
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {d.needing_repair ? "True" : "False"}
-                      </td>
+        {/* Table Starts here  */}
+        <div className="flex flex-col mt-5 mx-20 border-8">
+          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="overflow-hidden">
+                <table className="min-w-full text-center">
+                  <thead className="border-b">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Availability
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Durability
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Mileage
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Needing Repair
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-sm font-medium text-gray-900 px-6 py-4"
+                      >
+                        Minimum Rent Period
+                      </th>
                     </tr>
-                  </tbody>
-                ))}
-              </table>
+                  </thead>
+
+                  {modifiedData.map((d) => (
+                    <tbody>
+                      <tr className="border-b">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.name}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.availability ? "True" : "False"}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.price}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.durability}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.mileage}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.needing_repair ? "True" : "False"}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {d.minimum_rent_period}
+                        </td>
+                      </tr>
+                    </tbody>
+                  ))}
+                </table>
+              </div>
             </div>
           </div>
         </div>
+        {/* Table ends here */}
+
+        {/* Modal starts  here */}
+
+        <div className="mt-6 flex justify-end mr-20 mb-10">
+          <div className="mr-5">
+            <Modal />
+          </div>
+
+          <div className="mr-5">
+            <ModalReturn />
+          </div>
+        </div>
+        {/* modal ends here */}
       </div>
     </div>
   );
